7db32f99958eeeaed2750ffbe78fa612f1031809
xodud001 <freeptk1@gmail.com>
2021-05-04 22:46:03 +0900

    CORS 에러 Webpack에 Proxy 기능으로 해결

---
+++ src/component/Main/Learnrecord/Container/Container.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//34.64.73.179
+
+export function Container(props){
+
+    var [question, setQuestion] = useState(null);
+    const [query, setQuery] = useState("react");
+
+    var token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQG5hdmVyLmNvbSIsImlhdCI6MTYyMDEzNTIyNywiZXhwIjoxNjIwMTcxMjI3fQ.7VnprVPOKuqRUN2RUN2jgV-ca1duvfZhMojpMNKG8cs";
+    var fixedstring = encodeURIComponent(escape(token));
+    useEffect( () => {
+        let completed = false;
+        async function fetchData(){
+            axios({
+                method: 'get',
+                url: `/problem/question/5`,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json',
+                    "Authorization" : `Bearer ${fixedstring}`,
+                    withCredentials: true,
+                    mode: 'no-cors',
+                }
+            }).then(response => {
+                var data = response.data;
+                if(!completed) setQuestion(data)
+                console.log(data)
+            });
+        }
+        fetchData();
+        return () => {
+            completed = true;
+          };
+    }, [fixedstring]);
+
+    
+    if(!question) return `NULL`;
+
+    return(
+        <div className="board">{question.exam}</div>
+    );
+}
